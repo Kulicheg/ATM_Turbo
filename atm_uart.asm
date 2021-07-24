4727351af1b1620908fca77a7dee83db83db6c85
Kulicheg <41168303+Kulicheg@users.noreply.github.com>
2021-07-24 23:29:38 +0300

    Update atm_uart.asm

--- atm_uart.asm
+++ atm_uart.asm
@@ -1,29 +1,30 @@
     MODULE Uart
 		macro getqueue
-;		di
 		ld	a, #55	;подать комнаду контроллеру клавиатуры
 		in	a, (#0FE)
 		ld	a, #0C2	;команда - чтение счетчика буфера приема
 		in	a, (#0FE)
-;		ei
 		endm
 
 		macro getbyte
 ;Проверка готовности 80-100мкс, без нее плывем
-;		di
 chk_rec:
-		LD	BC,#55FE	;55FEh
-		IN	A,(C)		;Переход в режим команды
-		LD	B,#42
-		IN	A,(C)
+;		LD	BC,#55FE	;55FEh
+;		IN	A,(C)		;Переход в режим команды
+;		LD	B,#42
+;		IN	A,(C)
+		ld	a, #55	;подать комнаду контроллеру клавиатуры
+		in	a, (#0FE)
+		ld	a, #042	;команда - чтение счетчика буфера приема
+		in	a, (#0FE)
 		AND	01h		;RDY_RX(0)
-		bit	 0, a	
+;		bit	 0, a	
+
 		JR	Z,chk_rec; не готов? А теперь?	
-		LD	BC,#55FE	;55FEh
-		IN	A,(C)		;Переход в режим команды
-		LD	B,#02		;Чтение
-		IN	A,(C)
-;		ei
+		ld	a, #55	;подать комнаду контроллеру клавиатуры
+		in	a, (#0FE)
+		ld	a, #02	;команда - чтение счетчика буфера приема
+		in	a, (#0FE)
 		endm
 
 
@@ -36,7 +37,6 @@ init:
 		in	a,(#0FE)
 		ld	a,3	;параметр - установить скорость порта 19200(6) 38400(3) 115200(1) 57600(2) 9600(12) 14400(8)
 		in	a,(#0FE)
-		
 		call startrts
 		ei
 		ret
@@ -59,53 +59,6 @@ togetb:
 		pop bc
 		ei
 		ret	
-
-
-
-
-		
-fillbuf:
-		di
-		ld	e,40
-		ld	a,#55		;подать комнаду контроллеру клавиатуры
-		in	a,(#0FE)
-		ld	a,#43		;команда - установить статус
-		in	a,(#0FE)
-		ld	a, #03		;Параметры - убрать RTS (START)
-		in	a, (#0FE)
-
-fillbuf2:		
-		getqueue
-		cp 32
-		jp nc,fillbuf3
-		;getbyte
-chk_rec2:
-		LD	BC,#55FE	;55FEh
-		IN	A,(C)		;Переход в режим команды
-		LD	B,#42
-		IN	A,(C)
-		AND	01h		;RDY_RX(0)
-		bit	 0, a	
-		JR	Z,chk_rec2; не готов? А теперь?	
-		LD	BC,#55FE	;55FEh
-		IN	A,(C)		;Переход в режим команды
-		LD	B,#02		;Чтение
-		IN	A,(C)
-		
-		dec e
-		jp nz,fillbuf2	
-
-fillbuf3:		
-		ld	a,#55	;подать комнаду контроллеру клавиатуры
-		in	a,(#0FE)
-		ld	a,#43	;команда - установить статус
-		in	a,(#0FE)
-		ld	a,0	;Параметры - установить RTS (STOP)
-		in	a,(#0FE)
-		
-		ei
-		ret
-		
 		
 ; Write single byte to UART
 ; A - byte to write
@@ -165,7 +118,6 @@ startrts
 
 
 startrts2
-;		di
 		ld	a,#55		;подать комнаду контроллеру клавиатуры
 		in	a,(#0FE)
 		ld	a,#43		;команда - установить статус
@@ -179,13 +131,11 @@ startrts2
 		in	a,(#0FE)
 		ld	a,0			;Параметры - установить RTS (STOP)
 		in	a,(#0FE)
-;		ei
 		ret
 
 
 
 delay
-;		di
 		push de
 		ld e,0x01
 delay2
@@ -194,7 +144,6 @@ delay2
 		dec e
 		jr nz, delay2
 		pop de
-;		ei
 		ret
 
     ENDMODULE
