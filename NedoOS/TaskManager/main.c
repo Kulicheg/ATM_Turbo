6eb0a4fbb50ff49312ff3cc28252a81918762d7e
Kulicheg <41168303+Kulicheg@users.noreply.github.com>
2021-10-13 23:06:36 +0300

    Update main.c

--- NedoOS/TaskManager/main.c
+++ NedoOS/TaskManager/main.c
@@ -11,64 +11,56 @@ struct process
 {
   unsigned char nomer;
   unsigned char name[32];
+  unsigned char used;
   unsigned char window_0;
   unsigned char window_1;
   unsigned char window_2;
   unsigned char window_3;
 } table[16];
 
-    int procnum, prccount;
-    unsigned char c1, c2, pgbak;
-    unsigned char procname;
+int procnum, prccount;
+unsigned char c1, c2, pgbak, freemem, sysmem, usedmem;
+unsigned char procname;
 
-
-void initall(void)
-{
-  unsigned char c1, c2;
-  for (c1 = 1; c1 < 16; c1++)
-  {
-    table[c1].nomer    = 0;
-    table[c1].window_0 = 0;
-    table[c1].window_1 = 0;
-    table[c1].window_2 = 0;
-    table[c1].window_3 = 0;
-
-    for (c2 = 0; c2 < 32; c2++)
-    {
-      table[c1].name[c2] = 0;
-    }
-  }
-}
 
 void redraw(void)
 {
-    BOX(12, 4, 54, 1, 41);
-    AT(36, 4);
-    ATRIB(33);
-    puts("TASK MANAGER");
-    BOX(12, 5, 43, prccount, 43);
-    c2 = 1;
-    for (c1 = 1; c1 < 16; c1++)
+  BOX(12, 4, 54, 1, 41);
+  AT(36, 4);
+  ATRIB(33);
+  puts("TASK MANAGER");
+  BOX(12, 5, 43, prccount, 43);
+  c2 = 1;
+  for (c1 = 1; c1 < 16; c1++)
+  {
+    if  (table[c1].nomer != 0)
     {
-      if  (table[c1].nomer != 0)
-      {
-        AT(12, 5 + c2 - 1);
-        ATRIB(30);
-        putdec(table[c1].nomer);
-        putchar('.');
-        puts(table[c1].name);
+      AT(12, 5 + c2 - 1);
+      ATRIB(30);
+      putdec(table[c1].nomer);
+      putchar('.');
+      puts(table[c1].name);
 
 
-        AT(55, 5 + c2 - 1);
-        printf("%X.", table[c1].window_0);
-        printf("%X.", table[c1].window_1);
-        printf("%X.", table[c1].window_2);
-        printf("%X",  table[c1].window_3);
-        c2++;
-      }
+      AT(50, 5 + c2 - 1);
+      printf("%u  ", table[c1].used);
+      AT(55, 5 + c2 - 1);
+      printf("%X.", table[c1].window_0);
+      printf("%X.", table[c1].window_1);
+      printf("%X.", table[c1].window_2);
+      printf("%X",  table[c1].window_3);
+      c2++;
     }
+  }
+
+
+  BOX(12, 4 + c2, 54, 1, 41);
+  AT(14, 4 + c2);
+  ATRIB(33);
+  printf("  Free:%u pages     Used:%u pages  Sys:%u pages", freemem, usedmem, sysmem);
 
 }
+
 
 C_task main (int argc, char *argv[])
 {
@@ -79,7 +71,7 @@ C_task main (int argc, char *argv[])
 
     union APP_PAGES main_pg;
 
-//    initall();
+
     os_initstdio();
     main_pg.l = OS_GETMAINPAGES();
     pgbak = main_pg.pgs.window_3;
@@ -91,17 +83,19 @@ C_task main (int argc, char *argv[])
       if (errno == 0)
       {
         table[c1].nomer    = c1;
+        table[c1].used     = 0;
         table[c1].window_0 = main_pg.pgs.window_0;
         table[c1].window_1 = main_pg.pgs.window_1;
         table[c1].window_2 = main_pg.pgs.window_2;
         table[c1].window_3 = main_pg.pgs.window_3;
         SETPG32KHIGH(table[c1].window_0);
         memcpy(table[c1].name, (char*)(0xc000 + COMMANDLINE), 31);
-		prccount++;
+        prccount++;
       }
       else
       {
         table[c1].nomer    = 0;
+        table[c1].used     = 0;
         table[c1].window_0 = 0;
         table[c1].window_1 = 0;
         table[c1].window_2 = 0;
@@ -109,18 +103,43 @@ C_task main (int argc, char *argv[])
         table[c1].name[0] = '\0';
       }
     }
-	SETPG32KHIGH(pgbak);
 
-	redraw();
-	putchar('\r');
-	putchar('\n');
+    SETPG32KHIGH(pgbak);
+    freemem = 0;
+    sysmem  = 0;
+    usedmem = 0;
+    for ( c2 = 0; c2 < 255; c2++)
+    {
+      unsigned char owner;
+      owner = OS_GETPAGEOWNER(c2);
+      if (owner == 0)
+      {
+        freemem++;
+      }
+      else
+
+        if (owner == 255)
+        {
+          sysmem++;
+        }
+        else
+        {
+          table[owner].used++;
+          usedmem++;
+        }
+    }
+
+
+    redraw();
+    putchar('\r');
+    putchar('\n');
+
     procname = getchar();
 
     if  (procname > '0' && procname < '9')
     {
       procnum = procname - '0';
       OS_DROPAPP (procnum);
-      //  initall();
       BOX(12, 4, 58, prccount + 2, 40);
     }
     else if (procname == '\e')
@@ -128,7 +147,6 @@ C_task main (int argc, char *argv[])
       loop = 0;
     }
   }
-
   return 0;
 }
 
