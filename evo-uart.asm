f4e0a2571ad26fd6f71ce48815191cadc7cf51f5
Kulicheg <41168303+Kulicheg@users.noreply.github.com>
2021-11-05 10:50:47 +0300

    Update evo-uart.asm

--- evo-uart.asm
+++ evo-uart.asm
@@ -1,13 +1,9 @@
-; This driver works with 16c550 uart that's support AFE
     module Uart
-; Make init shorter and readable:-)
-    macro outp port, value
-
-	ld b, port
-	ld c, #EF
-    ld a, value
-	out (port), a
-	endm
+; This driver works with 16c550 uart that's support AFE
+;AT+UART_CUR=38400,8,1,0,0
+;AT+CWMODE_DEF=1
+;AT+CWJAP_DEF="Kulich","password"
+;
 
 ; Internal port constants
 RBR_THR equ #F8
@@ -19,73 +15,65 @@ LSR		equ	#FD
 MSR		equ	#FE
 SR		equ	#FF
 
+    macro outp port, value
+	di
+	ld b, port
+	ld c, #EF
+    ld a, value
+	out (port), a
+	ei
+	endm
+
+
+
 init:
 	push bc
-    outp MCR,     #0d  // Assert RTS
-    outp IIR_FCR, #87  // Enable fifo 8 level, and clear it
-    outp LCR,     #83  // 8n1, DLAB=1
-    outp RBR_THR, #03  // 115200 (divider 1)
-    outp IER,     #00  // (divider 0). Divider is 16 bit, so we get (#0002 divider)
-    outp LCR,     #03 // 8n1, DLAB=0
-    outp IER,     #00 // Disable int
-    outp MCR,     #2f // Enable AFE
+    outp MCR,     #0d	// Assert RTS
+    outp IIR_FCR, #87	// Enable fifo 8 level, and clear it
+    outp LCR,     #83	// 8n1, DLAB=1
+    outp RBR_THR, #03	// 115200 (divider 1-115200, 3 - 38400)
+    outp IER,     #00	// (divider 0). Divider is 16 bit, so we get (#0002 divider)
+    outp LCR,     #03	// 8n1, DLAB=0
+    outp IER,     #00	// Disable int
+    outp MCR,     #2f	// Enable AFE
 	pop bc
     ret
     
 ; Flag C <- Data available
 isAvailable:
-	ld a, LSR
-	in a, (#EF)
+	ld bc, #FDEF
+	in a, (c)
     rrca
-    ret
-
-; Non-blocking read
-; Flag C <- is byte was readen
-; A <- byte
-read:
-    ld a, LSR
-	in a, (#EF)
-    rrca
-    ret nc
-    ld a, RBR_THR
-	in a, (#EF)
-    scf 
-    ret
-
-; Tries read byte with timeout
-; Flag C <- is byte read
-; A <- byte
-readTimeout:
-    ld b, 10
-.wait
-    call isAvailable : jr c, read
-    halt
-    djnz .wait
-    or a
     ret
 
 ; Blocking read
 ; A <- Byte
-readB:
-    ld a, LSR
-	in a, (#EF)
+read:
+	di
+    ld bc, #FDEF
+	in a, (c)
     rrca
-    jr nc, readB
-    ld a, RBR_THR
-	in a, (#EF)
+    jr nc, read
+    ld bc, #F8EF
+	in a, (c)
+	ei	
     ret
 
 ; A -> byte to send
+
 write:
+	di
     push af
 .wait
-    ld a, LSR
-	in a, (#EF)
+    ld bc, #FDEF
+	in a, (c)
     and #20
     jr z, .wait
     pop af
-    ld a, RBR_THR 
-	out (#EF), a
+
+	ld bc,#F8EF
+	out (c),a	
+	ei
     ret
 
     endmodule
